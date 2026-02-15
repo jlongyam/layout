38155aaf3c196c159465f1b74d75f385b3e732b2
jlongyam <jhonliongbox@gmail.com>
2026-02-15 19:46:32 +0700

    feat: add tester iframe

--- test/display.js
+++ test/display.js
@@ -1,5 +1,17 @@
-function innerHTML(title = 'Demo') {
-  let txt = `
+var src = document.currentScript.getAttribute('data-src');
+var link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = `${src}/preview/style.css`;
+document.head.appendChild(link);
+
+var script = document.createElement('script');
+script.src = `${src}/preview/script.js`;
+document.head.appendChild(script);
+
+window.addEventListener('load', function () {
+
+  function innerHTML(title = 'Demo') {
+    let txt = `
     <header>
       <h1 title="title">✨ ${title}</h1>
     </header>
@@ -8,36 +20,41 @@ function innerHTML(title = 'Demo') {
       <button id="result">⚡ RESULT &nbsp;</button>
       <iframe id="preview"></iframe>
     </footer>`
-  ;
-  return txt;
-}
-
-document.body.innerHTML = innerHTML('Test');
-
-var title = document.getElementById('title');
-var main = document.getElementById('main');
-var result = document.getElementById('result');
-var preview = document.getElementById('preview');
-
-function displayHtml(content) {
-  var snippet = {
-    html: sourceCode({
-      open: true,
-      title: 'HTML',
-      id: 'txt_html',
-      content: stripInitial(content).trim()
-    })
-  };
-  main.innerHTML += snippet.html;
-  var txt_html = document.getElementById('txt_html');
-
-  function previewHtml() {
-     writeToIframe('preview', contentHtml({
-      link: '../src/index.css',
-      body: txt_html.value
-    }))
+      ;
+    return txt;
   }
-  window.addEventListener('load', previewHtml);
-  result.addEventListener('click', previewHtml);
-}
+
+
+  document.body.innerHTML = innerHTML('Test');
+  var title = document.getElementById('title');
+  var main = document.getElementById('main');
+  var result = document.getElementById('result');
+  var preview = document.getElementById('preview');
+
+  function displayHtml(content) {
+    var snippet = {
+      html: sourceCode({
+        open: true,
+        title: 'HTML',
+        id: 'txt_html',
+        content: stripInitial(content).trim()
+      })
+    };
+    main.innerHTML += snippet.html;
+    var txt_html = document.getElementById('txt_html');
+    function previewHtml() {
+      writeToIframe('preview', contentHtml({
+        link: `${src}/index.css`,
+        body: txt_html.value
+      }))
+    }
+    result.addEventListener('click', previewHtml);
+    previewHtml();
+  }
+  window.displayHtml = displayHtml;
+})
+
+
+
+
 
